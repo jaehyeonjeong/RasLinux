b3b4892a189f80fd91fa22623d991e4c8079aa0c
jaehyeonjeong <110385530+jaehyeonjeong@users.noreply.github.com>
2022-12-14 14:27:27 +0900

    update

--- linux_programming/readcamera.cpp
+++ linux_programming/readcamera.cpp
@@ -2,35 +2,36 @@
 using namespace std;
 using namespace cv;
 
-void put_string(Mat &frame, string text, Point pt, int value)
-{
-	text += to_string(value);
-	Point shade = pt + Point(2, 2);
-	int font = FONT_HERSHEY_SIMPLEX;
-	putText(frame, text, shade, font, 0.7, Scalar(0, 0, 0), 2);
-	putText(frame, text, pt, font, 0.7, Scalar(120, 200, 90), 2);
-}
-
 int main()
 {
 	VideoCapture capture(0);
-	if(!capture.isOpened())
-	{
-		cout << "카메라가 연결되지 않았습니다." << endl;
-		exit(1);
-	}
-	cout << "너비 " << capture.get(CAP_PROP_FRAME_WIDTH) << endl;
-	cout << "높이 " << capture.get(CAP_PROP_FRAME_HEIGHT) << endl;
-	cout << "노출 " << capture.get(CAP_PROP_EXPOSURE) << endl;
-	cout << "밝기 " << capture.get(CAP_PROP_BRIGHTNESS) << endl;
+	CV_Assert(capture.isOpened());
+
+	double fps = 29.97;
+	int delay = cvRound(1000.0 / fps);
+	Size size(640, 360);
+	int fourcc = VideoWriter::fourcc('D', 'X', '5', '0');
+
+	capture.set(CAP_PROP_FRAME_WIDTH, size.width);
+	capture.set(CAP_PROP_FRAME_HEIGHT, size.height);
+
+	cout << "width x height : " << size << endl;
+	cout << "VideoWriter::fourcc : " << fourcc << endl;
+	cout << "delay : " << delay << endl;
+	cout << "fps : " << fps << endl;
+
+	VideoWriter writer;
+	writer.open("../image/video_file.avi", fourcc, fps, size);
+	CV_Assert(writer.isOpened());
 
 	for(;;){
 		Mat frame;
-		capture.read(frame);
+		capture >> frame;
+		writer << frame;
 
-		put_string(frame, "EXPOS: ", Point(10, 40), capture.get(CAP_PROP_EXPOSURE));
 		imshow("카메라 영상보기", frame);
-		if(waitKey(30) >= 0) break;
+		if(waitKey(delay) >= 0)
+			break;
 	}
 	return 0;
 }
